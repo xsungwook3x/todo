abca8989cba02c5bc434d70febec17df86fe5943
Seonguk <xsungwook3x@naver.com>
2022-02-12 18:27:05 +0900

    java springboot를 이용한 서버 구성 시작

---
+++ src/main/java/com/example/demo/controller/TodoController.java
@@ -0,0 +1,49 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.ResponseDTO;
+import com.example.demo.dto.TodoDTO;
+import com.example.demo.model.TodoEntity;
+import com.example.demo.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("todo")
+public class TodoController {
+
+    @Autowired
+    private TodoService service;
+
+    @PostMapping
+    public ResponseEntity<?> createTodo(@RequestBody TodoDTO dto){
+        try{
+            String temporaryUserId="temporary-user";
+
+            TodoEntity entity = TodoDTO.toEntity(dto);
+
+            entity.setId(null);
+
+            entity.setUserId(temporaryUserId);
+
+            List<TodoEntity> entities = service.create(entity);
+
+            List<TodoDTO> dtos= entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+
+            ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
+
+            return ResponseEntity.ok().body(response);
+
+        } catch (Exception e){
+            String error =e.getMessage();
+            ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().error(error).build();
+            return ResponseEntity.badRequest().body(response);
+        }
+    }
+}
---
+++ src/main/java/com/example/demo/service/TodoService.java
@@ -0,0 +1,51 @@
+package com.example.demo.service;
+
+import com.example.demo.model.TodoEntity;
+import com.example.demo.persistence.TodoRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@Service
+public class TodoService {
+
+    @Autowired
+    private TodoRepository repository;
+
+    public String testService(){
+        //TodoEntity 생성
+        TodoEntity entity = TodoEntity.builder().title("My first todo item").build();
+        //TodoEntity 저장
+        repository.save(entity);
+        //TodoEntity 검색
+        TodoEntity savedEntity=repository.findById(entity.getId()).get();
+
+        return savedEntity.getTitle();
+    }
+
+    public List<TodoEntity> create(final TodoEntity entity){
+        //Validations
+        validate(entity);
+
+        repository.save(entity);
+
+        log.info("Entity Id : {} is saved.", entity.getId());
+
+        return repository.findByUserId(entity.getUserId());
+    }
+
+    private void validate(final TodoEntity entity){// refactoring
+        if(entity ==null) {
+            log.warn("Enriry cannot be null.");
+            throw new RuntimeException("Entity cannot be null");
+        }
+
+        if(entity.getUserId() == null){
+            log.warn("Unknown user.");
+            throw new RuntimeException("Unknown user.");
+        }
+    }
+}
