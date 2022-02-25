23a5dfc154be57f4152603b70184bed398788dcd
Seonguk <xsungwook3x@naver.com>
2022-02-25 23:02:32 +0900

    security2

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -4,6 +4,7 @@ import com.example.demo.model.UserEntity;
 import com.example.demo.persistence.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -26,7 +27,12 @@ public class UserService {
         return userRepository.save(userEntity);
     }
 
-    public UserEntity getByCredentials(final String email, final String password){
-        return userRepository.findByEmailAndPassword(email,password);
+    public UserEntity getByCredentials(final String email, final String password, final PasswordEncoder encoder){
+        final UserEntity originalUser=userRepository.findByEmail(email);
+
+        if(originalUser != null && encoder.matches(password,originalUser.getPassword())){
+            return originalUser;
+        }
+        return null;
     }
 }
